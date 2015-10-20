d38e893bdb10ad972e77cbd887ee964d8e038806
xiangzhendong <zhendong.xiang@gmail.com>
2015-10-20 22:30:39 +0800

    Update main.py

---
+++ _src/om2py0w/0wex0/main.py
@@ -0,0 +1,8 @@
+# *-* coding:utf-8 *-*
+# quick python script explanation for programmers
+import os
+def main():
+    print "hello world"
+    print "这是Alice\'的问候。"
+    print "这是Bob\'的问候。"
+    foo(5,10)
