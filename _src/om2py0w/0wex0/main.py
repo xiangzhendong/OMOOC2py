b8969a5400019ef31b7b5de56dcb2b48a8f5bfbe
xiangzhendong <zhendong.xiang@gmail.com>
2015-10-22 20:47:46 +0800

    Update main.py

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -3,6 +3,38 @@
 import os
 def main():
     print "hello world"
+    
     print "这是Alice\'的问候。"
     print "这是Bob\'的问候。"
+    
     foo(5,10)
+    
+    print '='*10
+    print '这将直接执行'＋os.getcwd()
+    
+    counter=0
+    counter+=1
+    
+    food=['苹果'，‘杏子’，‘李子’，‘梨’]
+    for i in food:
+        print '俺就爱整只：'＋i
+        
+    print '数到10'
+    for i in range(10):
+        print i
+        
+def foo(param1,sencondparam):
+    res=param1+secondparam
+    print '%s 加 %s等于 %s'%(param1,secondparam,res)
+    if res<50:
+        print '这个'
+    elif (res>=50) and ((param1==42) or (secondparam==24)):
+        print '那个'
+    else:
+        print '嗯...'
+    return res
+    """这是多行
+    注释......"""
+    
+if _name_=='_main_':
+    main()
