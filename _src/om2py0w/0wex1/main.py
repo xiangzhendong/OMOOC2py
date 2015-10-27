da85832e670ab0c2c27debf5b1d67e6b157ac2f5
xiangzhendong <zhendong.xiang@gmail.com>
2015-10-27 22:54:00 +0800

    Update main.py

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,11 +1,15 @@
  # -*- coding:utf-8 -*-
+ for line in open('mydiary.txt'):
+     print(line,end='')
+ 
  mydiary=open('mydiary.txt','a')
  while True:
      content=input('-->')
      if content=='q':break
      mydiary.write(content+'\n')
-  mydiary.close()
+ mydiary.close()
   
-  for line in open('mydiary.txt'):
-      print(line,end='')
-      
+ if __name__=="__main__"
+     main()
+     
+     
